3882e0cd1a964c2c71cc5da43d1e07ac8ca7414d
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-10-23 23:34:59 +0500

    Initial commit

---
+++ src/Child.js
@@ -0,0 +1,87 @@
+import React, { useContext, useState } from 'react';
+import { TransactionContext } from './transContext';
+
+
+function Child() {
+  let { transactions, addTransaction } = useContext(TransactionContext);
+  let [newDesc,  setDesc] = useState("");
+  let [newAmount,  setAmount] = useState("0");
+
+ const handleAddition = (event)=> {
+  event.preventDefault();
+  if(Number(newAmount) === 0){
+  alert ("please enter correct value")
+  return false ;
+  }
+  addTransaction({
+    amount : Number(newAmount),
+    desc :  newDesc
+  });
+  setDesc ("");
+  setAmount (0)
+
+ }
+ 
+ const getIncome =() =>{
+   let income = 0;
+   for (var i = 0 ; i < transactions.length; i++ ){
+     if( transactions[i].amount > 0)
+     income = income + transactions[i].amount
+   }
+return income;
+ } 
+
+ const getExpense =() =>{
+   let expense = 0;
+   for (var i = 0; i< transactions.length; i++) {
+     if (transactions[i].amount < 0 )
+     expense += transactions[i].amount
+   }
+   return expense;
+ }
+  return (
+    <div className="container">
+      <h1 className="text_cenetr">Expence Tracker</h1>
+      <h3>your Balance <br /> ${getIncome()  + getExpense()} </h3>
+
+      <div className="expence_container">
+        <h3>Income <br /> ${getIncome()}</h3>
+        <h3>Expence <br /> ${getExpense()} </h3>
+      </div>
+      <h3>History</h3>  
+      <hr />
+
+      <ul className="transaction-list">
+        {transactions.map((transObj, ind) => {
+          return (<li key={ind}>
+            <span>{transObj.desc}</span>
+            <span>${transObj.amount}</span>
+
+          </li>);
+        })}
+      </ul>
+      <h3>Add new transaction</h3>
+      <hr />
+
+      <form className="transaction-form" onSubmit={handleAddition} >
+        <label>Enetr Description <br />
+          <input type="text" 
+          value={newDesc}
+          placeholder="Descripton"
+          onChange={(ev)=>setDesc(ev.target.value)} required  />
+        </label><br />
+        <label>Enter Amount <br />
+          <input type="number"
+          value={newAmount}
+          placeholder="Amount"
+           onChange={(ev)=>setAmount(ev.target.value)} required />
+        </label><br />
+
+        <input type="submit" value="Add Transaction" />
+      </form>
+    </div>
+
+  );
+}
+
+export default Child;
